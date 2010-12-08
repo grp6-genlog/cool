cc07366383ce29d0fc828ee420c9cd0c568e986c
mart <mart@mart.net>
2010-12-08 18:05:43 +0100

    undo __init__ modification

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,3 @@
-"""
 import sys, os
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
@@ -28,5 +27,4 @@ proposal_rec_port = ProposalRecorder(find_pair_port)
 request_rec_port = RequestRecorder(find_pair_port)
 
 print "Car Pooling Server is now runing"
-"""
-print "starting server"
+
