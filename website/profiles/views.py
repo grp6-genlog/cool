3a6469d42b90afa2c0f23f8b36df04754b1c1107
mart <mart@mart.net>
2010-12-12 12:29:49 +0100

    registration validation

--- website/profiles/views.py
+++ website/profiles/views.py
@@ -4,10 +4,20 @@ from django.contrib import auth
 from django.core.context_processors import csrf
 from django.template import RequestContext
 from django.contrib.admin import widgets
+from django.db import models
+
 
 from website.profiles.models import UserProfile
+from django.contrib.auth.models import User
 
 import datetime
+
+
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
+
 
 class RegisterForm(forms.Form):
     username = forms.CharField(max_length=50,)
@@ -18,20 +28,23 @@ class RegisterForm(forms.Form):
                        label=u'Confirm password')
     email = forms.EmailField(max_length=70,
                         label=u'Email address')                                              
-    number_of_seats = forms.IntegerField()
     date_of_birth = forms.DateField(widget=widgets.AdminDateWidget())
-    smoker = forms.BooleanField()
-    communities = forms.CharField(max_length=100)
-    money_per_km = forms.FloatField()
-    gender = forms.CharField(max_length=1)
+    smoker = forms.BooleanField(required=False)
+    gender = forms.ChoiceField(choices=GENDER_CHOICES)
+    communities = forms.CharField(max_length=100,
+                        required=False)
+    money_per_km = forms.FloatField(required=False)
     bank_account_number = forms.CharField(max_length=30)
-    account_balance = forms.FloatField()
-    car_id = forms.CharField(max_length=50,
-                        label=u'Car plate number')
     phone_number = forms.CharField(max_length=20)
+    car_id = forms.CharField(max_length=10,
+                        label=u'Car plate number',
+                        required=False)
     car_description = forms.CharField(max_length=500,
-                            widget=forms.Textarea,)
-    smartphone_id = forms.CharField(max_length=100)
+                            widget=forms.Textarea,
+                            required=False)
+    number_of_seats = forms.IntegerField(required=False)
+    smartphone_id = forms.CharField(max_length=100,
+                                required=False)
 
 def register(request):
     
@@ -40,5 +53,26 @@ def register(request):
         name = request.user.username
         return render_to_response('index.html', locals())
     else:
-        form = RegisterForm(initial={'date_of_birth': datetime.date.today()})
+        if request.method == 'POST':
+            form = RegisterForm(request.POST)
+               
+            valid = form.is_valid()
+            
+            usern = request.POST.get('username', '')
+            if len(User.objects.filter(username=usern))!=0:
+                form._errors["username"] = form.error_class(["There is already a user with that username"])
+                valid = False
+            
+            passwd = request.POST.get('password', '')
+            passwd2 = request.POST.get('password2', '')
+            if passwd != passwd2:
+                form._errors["password"] = form.error_class(["The two passwords are different"])
+                valid = False
+            
+            if valid:
+                #utils.toprofilerecorder(request,'register')
+                notif = 'Registered !'
+
+        else:
+            form = RegisterForm(initial={'date_of_birth': datetime.date.today()})
         return render_to_response('register.html', locals())
