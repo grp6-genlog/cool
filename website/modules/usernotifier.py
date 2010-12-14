6b7bdd238064ff4b5095f9f05873fb06dcd626c2
fiacre <sfiacre@gmail.com>
2010-12-14 12:43:53 +0100

    usernotifier

--- website/modules/usernotifier.py
+++ website/modules/usernotifier.py
@@ -2,36 +2,59 @@
 # Interface of the UserNotifier module
 
 from portobject import *
+import smtplib  
 
 class UserNotifier(PortObject):
-    
+	
+	dbconn = None # Connection to the Data Base for reach GSM number or email address from desired user based in userID
 
-    def __init__(self):
+	def __init__(self,db):
         """
-        Initialize the module
-        @pre db is the SQL database of our system
-        @post self.dbconn=db
-              initialize the port object
-        """
+	Initialize the module
+	@pre db is the SQL database of our system
+	@post self.dbconn=db
+	      initialize the port object
+	"""
         PortObject.__init__(self)
 
-    def SendMessageToUser(self,userID=None,message=None):
-        """
-        Sends a notification to user thru a desired medium
-        @pre:userID is the user to send the notification
-            message is the message to send
-        
-        @post:    The message has been sent to user using his desired device
-        """
-        pass
+
+	def SendMessageToUser(self,db,userID=None,message=None)
+	"""
+	Sends a notification to user thru a desired medium
+	@pre:	dbconn is the SQL database
+		userID is the user to send the notification
+		message is the message to send
+	
+	@post:	The message has been sent to user using his desired device
+	"""
+        u= UserProfile.objects.get(user=userID)
+        mail=u.email
+          
+        fromaddr = 'carpooling@gmail.com'  
+        toaddrs  = 'mail'  
+        username = 'username'  
+        password = 'password'  
+    
+        server = smtplib.SMTP('smtp.gmail.com:587')  
+        server.starttls()  
+        server.login(username,password)  
+        server.sendmail(fromaddr, toaddrs, message)  
+        server.quit()  
 
 
-    def routine(self,msg):
+	def routine(self,msg):
         """
-    The only acceptable msg is ('newmsg',userID,strmessage)
-    @pre : userID is the id of a user in the dbconn
-           strmessage is a string
+	The only acceptable msg is ('newmsg',userID,strmessage)
+	@pre : dbconn is initialized
 
-    @post : the send_message_to_user is called.
-    """
-        pass
+	       userID is the id of a user in the dbconn
+	       strmessage is a string
+
+	@post : the send_message_to_user is called.
+	"""
+        if msg[0]=='newmsg':
+          try:
+              userID=msg[1]
+              message=msg[2]
+              SendMessageToUser(db,userID,message)
+          excep
