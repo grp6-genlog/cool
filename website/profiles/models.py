4ae2f23bc76fa605bc1d824d949cb3fe0b9ecfc3
mart <mart@mart.net>
2010-12-15 09:18:42 +0100

    update db

--- website/profiles/models.py
+++ website/profiles/models.py
@@ -14,17 +14,17 @@ class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     
     number_of_seats = models.IntegerField()
-    date_of_birth = models.DateField(blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
     smoker = models.BooleanField(default=False, blank=True)
-    communities = models.CharField(max_length=100, blank=True)
-    money_per_km = models.FloatField(blank=True)
+    communities = models.CharField(max_length=100, blank=True, null=True)
+    money_per_km = models.FloatField(blank=True, null=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     bank_account_number = models.CharField(max_length=30)
-    account_balance = models.FloatField(default=0.0, blank=True)
-    car_id = models.CharField(max_length=50, blank=True)
+    account_balance = models.FloatField(default=0.0)
+    car_id = models.CharField(max_length=50, blank=True, null=True)
     phone_number = models.CharField(max_length=20)
-    car_description = models.TextField(max_length=500, blank=True)
-    smartphone_id = models.CharField(max_length=100, blank=True)
+    car_description = models.TextField(max_length=500, blank=True, null=True)
+    smartphone_id = models.CharField(max_length=100, blank=True, null=True)
     
     def __str__(self):  
           return "%s's profile" % self.user
