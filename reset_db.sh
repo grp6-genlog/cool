116d8d53cc4665f495ab24cd78ee323ec58ce67c
mart <mart@mart.net>
2010-12-22 10:41:11 +0100

    cleaning

---
+++ reset_db.sh
@@ -0,0 +1,9 @@
+python manage.py reset evaluations
+python manage.py reset offers
+python manage.py reset profiles
+python manage.py reset proposals
+python manage.py reset requests
+python manage.py reset rides
+python manage.py reset proposals
+
+python manage.py createsuperuser --username=mart --email=mart@mart.net
